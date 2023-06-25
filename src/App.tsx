fef72455ca2e2a4d72dd6e666eee77f2b955302c
SuzukiTakamasa <69415488+SuzukiTakamasa@users.noreply.github.com>
2023-06-25 16:22:11 +0900

    add Nav and Banner

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import './App.css';
-import { requests } from './request';
 import { Row } from './components/Row';
+import { Banner } from "./components/Banner";
+import { Nav } from "./components/Nav";
+import { requests } from './request';
 
 function App() {
   return (
     <div className="App">
+      <Nav />
+      <Banner />
       <Row
         title="NETFLIX ORIGINALS"
         fetchUrl={requests.feachNetflixOriginals}
